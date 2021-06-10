4a03544c16fb1b88d9cb4e64bcceb498beb31129
ryan <ryanpegoud@gmail.com>
2021-06-10 10:45:27 +0200

    .......

--- app/main_menu.py
+++ app/main_menu.py
@@ -363,6 +363,7 @@ class Ui_MainMenu(object):
 "font: 32pt \"Gill Sans MT\";")
         #self.camera_select_label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.camera_select_label.move(10,170)
+        self.camera_select.move(10,220)
         self.camera_select_label.setObjectName("camera_select_label")
         self.horizontalLayout.addWidget(self.camera_select_label)
         self.camera_select = QtWidgets.QComboBox(self.horizontalLayoutWidget_2)
