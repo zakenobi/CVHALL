f2bf939bd30b1073b196a992e318c8a24097eaec
ryan <ryanpegoud@gmail.com>
2021-06-14 16:19:55 +0200

    .

--- exemple_programe/plot.py
+++ exemple_programe/plot.py
@@ -5,16 +5,18 @@ import matplotlib.pyplot as plt
 path = ("exemple_programe\stat_CS\donnee.csv")
 df = pd.read_csv(path,sep=';',index_col=0)
 #print(df.head())
-dp = df[['nb_masques_bien_portes','nb_masques_mal_portes']].iloc[[0]]
+dp = df[['nb_masques_bien_portes','nb_masques_non_portes']]
 #print(dp)
-labels = ['nb_masques_bien_portes','nb_masques_mal_portes']
+labels = ['nb_masques_bien_portes','nb_masques_non_portes']
 fig = plt.figure()
 
 slices = [dp.iloc[0][0],dp.iloc[0][1]]
 labels = [dp.iloc[0][0],dp.iloc[0][1]]
-
+print(dp)
+print(dp.iloc[[0]].sum())
 plt.pie(slices, labels = labels)
 plt.title('Proportion de sujets portant leur masque ou non')
-plt.legend(['Masque porté','Masque non porté'],loc = "lower left", facecolor = "lightgray")
+plt.legend(['Masque porté','Masque non porté'],loc = "lower right", facecolor = "lightgray")
 plt.savefig("app/resources/Figure")
+
 plt.show()
