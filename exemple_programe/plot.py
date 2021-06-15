824840f10b1698585edb8ca753a69301e934e17d
Mathieu Laversin <mathieulaversin @outlook.fr>
2021-06-15 11:17:13 +0200

    update

--- exemple_programe/plot.py
+++ exemple_programe/plot.py
@@ -1,20 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import pie, axis, show 
+import pandas as pd
 
-
-path = ("exemple_programe\stat_CS\donnee.csv")
-df = pd.read_csv(path,sep=';',index_col=0)
+path = ("/Users/mathieu/ProjetChallenge/CVHALL/exemple_programe/stat_CS/donnee.csv")
+df = pd.read_csv(path,sep=';',index_col=1)
 #print(df.head())
-dp = df[['nb_masques_bien_portes','nb_masques_mal_portes']].iloc[[0]]
+dp = df[['Somme_avec_masques','Somme_non_masques']].iloc[[0]]
 #print(dp)
-labels = ['nb_masques_bien_portes','nb_masques_mal_portes']
+labels = ['Somme_avec_masques','Somme_non_masques']
 fig = plt.figure()
 
-slices = [dp.iloc[0][0],dp.iloc[0][1]]
-labels = [dp.iloc[0][0],dp.iloc[0][1]]
 
-plt.pie(slices, labels = labels)
-plt.title('Proportion de sujets portant leur masque ou non')
-plt.legend(['Masque porté','Masque non porté'],loc = "lower left", facecolor = "lightgray")
-plt.savefig("app/resources/Figure")
+labels = [dp.iloc[0][0],dp.iloc[0][1]]
+sum1 = dp.iloc[0][0].sum()
+sum2= dp.iloc[0][1].sum()
+slices = [sum1,sum2]
+#plt.pie(slices, labels = labels)
+#plt.title('Proportion de sujets portant leur masque ou non')
+#plt.legend(['Masque porté','Masque non porté'],loc = "lower left", facecolor = "lightgray")
+#plt.savefig("app/resources/Figure")
+
+#Histogramme 
+
+hours = df.groupby('Heures').agg('sum')
+hours = hours[['nb_masques_bien_portes','nb_masques_non_portes']]
+print(hours)
+#plt.hist(x=hours.index.values)
+plt.hist(hours)
+hours.plot.bar()
+print(df.index.values)
 plt.show()
