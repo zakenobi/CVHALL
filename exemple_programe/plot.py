dd0303ce8f7f623ff5c4e59f5cd64fe88df5871d
ryan <ryanpegoud@gmail.com>
2021-06-17 13:07:08 +0200

    time sans minute,secondes

--- exemple_programe/plot.py
+++ exemple_programe/plot.py
@@ -13,7 +13,7 @@ path = ("exemple_programe/stat_CS/donnee.csv")
 df = pd.read_csv(path,sep=';',index_col=1)
 dp = df[['Somme_avec_masques','Somme_non_masques']].iloc[[0]]
 
-fig = plt.figure(figsize=(8, 6), dpi=80)
+# fig = plt.figure(figsize=(8, 6), dpi=80)
 
     # Pie
 
@@ -25,12 +25,12 @@ sum_masque = df['Somme_avec_masques'] = df['nb_masques_bien_portes'].sum()
 sum_Nmasque = df['Somme_sans_masques'] = df['nb_masques_non_portes'].sum()
 
 
-labels = [sum_masque,sum_Nmasque]
-slices = [sum_masque,sum_Nmasque]
-plt.pie(slices, labels = labels)
-plt.title('Proportion de sujets portant leur masque ou non')
-plt.legend(['Masque porté','Masque non porté'],loc = "lower left", facecolor = "lightgray")
-plt.savefig("app/resources/Pie")
+# labels = [sum_masque,sum_Nmasque]
+# slices = [sum_masque,sum_Nmasque]
+# plt.pie(slices, labels = labels)
+# plt.title('Proportion de sujets portant leur masque ou non')
+# plt.legend(['Masque porté','Masque non porté'],loc = "lower left", facecolor = "lightgray")
+# plt.savefig("app/resources/Pie")
 
 #Histogramme 
 hours = df.groupby('Heures').agg('sum')
