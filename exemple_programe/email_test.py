950a2a18761eab08846701b793a93491cb7d6777
zakenobi <vanvansurferman@gmail.com>
2021-06-21 14:35:40 +0200

    Clean files

--- exemple_programe/email_test.py
+++
@@ -1,4 +0,0 @@
-from mailer import Mailer
-
-mail = Mailer(email='cvhall.epf@gmail.com', password='PMFGE3A.23')
-mail.send(receiver='zac.gagnou@gmail.com', subject='ALERT', message='Individu dangereux demasquer !')
--- exemple_programe/write csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from pathlib import Path
-from matplotlib.pyplot import pie, axis, show, figure
-from pandas.core.frame import DataFrame
-
-dc = DataFrame(columns=['Date','Heures','nb_masques_bien_portes','nb_masques_non_portes','Somme_avec_masques','Somme_non_masques'])
-dc.to_csv(r"exemple_programe\stat_CS\donnee.csv",  index = False,sep=';', encoding='utf-8')
